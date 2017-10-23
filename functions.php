bd901fbc760743a708a6ab0b69871fd38f129c1f
AdamSkuse <a.t.skuse@gmail.com>
2017-10-22 20:57:59 -0400

    Adds grid of latest six posts to index.php

--- functions.php
+++ functions.php
@@ -4,3 +4,8 @@ add_action( 'wp_enqueue_scripts', 'get_assets' );
 function get_assets() {
 	wp_enqueue_style( 'hedgehogs', get_stylesheet_uri() );
 }
+
+function custom_excerpt_length( $length ) {
+	return 20;
+}
+add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
--- index.php
+++ index.php
@@ -1,3 +1,36 @@
 <?php get_header() ?>
-<p>Body</p>
+
+<!-- posts grid 
+<section class="posts-grid">
+  <div class="post-1">1</div>
+  <div class="post-2">2</div>
+  <div class="post-3">3</div>
+  <div class="post-4">4</div>
+  <div class="post-5">5</div>
+  <div class="post-6">6</div>
+</section>
+/posts grid -->
+
+<?php if( have_posts() ) : ?>
+  <?php $postcount = 1 ?> 
+  <section class="posts-grid">
+
+  <?php while( have_posts() ) : the_post() ?>
+    <?php if($postcount <=6) : ?>
+      <div class="post-<?php echo $postcount ?>"><h2><a href='<?php the_permalink() ?>'><?php the_title() ?></a></h2>
+      <div class="content">
+        <?php the_excerpt() ?>
+      </div>
+      </div>
+      <?php $postcount ++ ?>
+    <?php else : ?>
+    <?php endif ?>
+
+	<?php endwhile ?>
+
+</section>
+<?php else : ?>
+	<p>Oh No, there are no posts!</p>
+<?php endif ?>
+
 <?php get_footer() ?>
