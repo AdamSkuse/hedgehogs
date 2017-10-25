9befc1ba8c414600e4ffe7f10bc4f6f71fc7de3e
AdamSkuse <a.t.skuse@gmail.com>
2017-10-25 19:56:29 -0400

    Splits CSS into modules; updates gulp watch, buid and style tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,38 @@
 var gulp = require('gulp'),
-  watch = require('gulp-watch');
+  watch = require('gulp-watch'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  cssvars = require('postcss-simple-vars'),
+  nested = require('postcss-nested'),
+  cssImport = require('postcss-import'),
+  mixins = require('postcss-mixins');
 
-gulp.task('build', function() {
+gulp.task('build', ['styles'], function() {
   console.log("building");
+
   var filesToCopy = [
-  './style.css',
-  './*.php'
+    './*.php',
+    './temp/style.css'
   ]
 
   return gulp.src(filesToCopy)
   .pipe(gulp.dest("/Applications/MAMP/htdocs/wordpress/wp-content/themes/hedgehog"));
+
 });
 
 gulp.task('watch', function() {
   var filesToWatch = [
-  './style.css',
+  './dev/styles/**/*.css',
   './*.php'
   ]
 
   watch(filesToWatch, function() {
     gulp.start('build');
   });
+});
+
+gulp.task('styles', function() {
+  return gulp.src('./dev/styles/style.css')
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./temp'));
 });
